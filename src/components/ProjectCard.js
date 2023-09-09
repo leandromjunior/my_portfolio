d1061b4e13f05a750da7cf4a17e74425caa8bfd6
Leandro Motta Junior <lemottajr@hotmail.com>
2023-09-08 22:17:50 -0300

    inserção do parametro link para clique na imagem do projeto

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Col } from "react-bootstrap"
-export const ProjectCard = ({title, description, imgUrl}) => {
+export const ProjectCard = ({title, description, imgUrl, link}) => {
     return (
-        <Col sm={6} md={5}>
+        <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
                 <img src={imgUrl} />
                 <div className="proj-txtx">
-                    <h4>{title}</h4>
+                    <a href={link} target="_blank" rel="noopener noreferrer"><h4>{title}</h4></a>
                     <span>{description}</span>
                 </div>
             </div>
